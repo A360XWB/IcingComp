a6e24c1de57d3b8f0ef5c4aace1ce89eff2c9304
A360XWB <li.xcin.chen@gmail.com>
2024-03-19 02:27:34 -0400

    Array for real number created to test the read from .dpm file (string) and write the transferred data.

--- readDPM.f95
+++ readDPM.f95
@@ -1,14 +1,17 @@
 program readDPM
   implicit none
-  character(len = 50) :: filename = "TestData.dpm" ! Specify your input file name
-  integer, parameter :: num_col = 13, num_row = 10000000
-  integer :: iostat, i, len_str
+  character(len = 50) :: filename = "800plane-11=4.5-test.dpm" ! Specify your input file name
+  integer, parameter :: num_col = 12, num_row = 10000000
+  integer :: iostat, i, j, k, len_str
   character(len = 500) :: string
   character(len = 20) :: properties_C
-
+  
+  real :: properties 
+  dimension properties (0 : num_row, 0 : num_col)
+  
   ! Store properties from xyz, uvw, diameter, t, parcel-mass, mass, n-in-parcel, time, to flow time
-  real*8 properties   ! 13 properties in total
-  dimension properties (0 : num_col, 0 : num_row)
+  ! 13 properties in total
+
 
   ! Open the file for reading
   open(unit = 10, file = filename, status = 'old', action = 'read', iostat = iostat)
@@ -19,35 +22,50 @@ program readDPM
   ! Neglect first two lines
   read (10, *)
   read (10, *)
-  ! Read the space-separated strings
+  
+  j = 0
   do
-    read(10, '(A)', iostat=iostat) string
+    write(*, *) 'j = ', j
+    read(10, '(A)', iostat=iostat) string   ! Read the space-separated strings
     if (iostat /= 0) then 
       exit  ! Exit loop at the end of the file
     elseif (trim(string) == ')') then 
       exit
     end if
-    write(*, *) string
+    !write(*, *) string ! string is being read
 !go to 777
-    len_str = len_trim(string)
+    len_str = len_trim(string) ! extract data from the string
+    k = 0
     do i = 0, len_str
-      if (string(i:i) == ")") then 
+      if (k == num_col + 1) then
         exit
       else if (string(i:i) >= '0' .and. string(i:i) <= '9') then
         properties_C = string(i : i + 9)
-        write(*, *) properties_C
+        read(properties_C, *) properties (j, k)
         call my_incr(i, 9)
+        call my_incr(k, 1)
       else if (string(i:i) == '-') then
         properties_C = string(i : i + 10)
-        write(*, *) properties_C
+        read(properties_C, *) properties (j, k)
         call my_incr(i, 10)
+        call my_incr(k, 1)
       end if
-      !write(*, *) properties_C
+      !write(*, *) i, j, k, ' element: ', properties_C 
     end do
+    call my_incr(j, 1)
   end do
 !777 continue
   ! Close the file
   close(10)
+
+do i = 0, 30
+  do k = 0, num_col
+    write(*, *) 'P(', i, ', ', k, ') = ', properties(i, k)    
+  end do
+end do
+
+!write(*, *) 'P(0, 5) * P(0, 6) = ', properties(0, 5) * properties(0, 6)
+
 end program readDPM
 
 subroutine my_incr(var, incr)
